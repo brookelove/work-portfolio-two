df544a1c23b0bada7c3b67d83ca2b7132ca980a6
Brooke Love <savienloveb@gmail.com>
2022-04-15 23:11:12 -0700

    adding memoji images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
 console.log("I am linked");
 // HTML elements 
 var navbar = document.getElementById("navbar");
-var nav__list = document.getElementById("nav__list");
+var nav__list = document.getElementById("header");
+var nav__title = document.getElementById("nav__title");
 
 // Sticky element
 window.onscroll = function (){
     if (window.pageYOffset >= nav__list.offsetTop){
         navbar.classList.add("sticky");
+        nav__title.classList.add("sticky");
     } else {
         navbar.classList.remove("sticky");
+        nav__title.classList.remove("sticky");
     }
 }
