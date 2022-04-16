1bc9f7a82ef49b7faaef740fa7a74b0f532a197a
Brooke Love <savienloveb@gmail.com>
2022-04-16 10:11:01 -0700

    setting up about me placement

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,15 @@ console.log("I am linked");
 // HTML elements 
 var navbar = document.getElementById("navbar");
 var nav__list = document.getElementById("header");
-var nav__title = document.getElementById("nav__title");
+
+// var nav__title = document.getElementById("nav__title");
 
 // Sticky element
+    // try to get sticky element to work on all elements
 window.onscroll = function (){
     if (window.pageYOffset >= nav__list.offsetTop){
         navbar.classList.add("sticky");
-        nav__title.classList.add("sticky");
     } else {
         navbar.classList.remove("sticky");
-        nav__title.classList.remove("sticky");
     }
 }
