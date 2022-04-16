b63a371ef3698c9784124bc7f70e819079d3bc3b
Brooke Love <savienloveb@gmail.com>
2022-04-15 22:15:02 -0700

    adding sticky navigation bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
 console.log("I am linked");
+// HTML elements 
+var navbar = document.getElementById("navbar");
+var nav__list = document.getElementById("nav__list");
+
+// Sticky element
+window.onscroll = function (){
+    if (window.pageYOffset >= nav__list.offsetTop){
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
